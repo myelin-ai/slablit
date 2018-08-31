bb874a5f125ec327340afd4dee0f5046995f51b9
Jeremy Stucki <jeremy@myelin.ch>
2018-08-31 14:53:26 +0200

    Fix build

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(decl_macro)]
+#![feature(decl_macro, macro_at_most_once_rep)]
 
 #[allow(unused_macros)]
 macro replace_expr($_t:tt $sub:expr) {
@@ -30,6 +30,18 @@ mod test {
     #[test]
     fn test_slab_macro() {
         let slab: Slab<usize> = slab![10, 20, 30];
+
+        assert_eq!(
+            vec![10, 20, 30],
+            slab.into_iter()
+                .map(|(_, value)| *value)
+                .collect::<Vec<usize>>()
+        );
+    }
+
+    #[test]
+    fn test_slab_macro_with_trailing_comma() {
+        let slab: Slab<usize> = slab![10, 20, 30, ];
 
         assert_eq!(
             vec![10, 20, 30],
